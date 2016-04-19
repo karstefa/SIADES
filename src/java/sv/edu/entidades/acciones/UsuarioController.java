a78d79ec069a5fdb052d84014af654e61f9d39c8
karstefa <ks.melara@gmail.com>
2016-04-18 22:09:35 -0600

    Configuraciones de template

--- src/java/sv/edu/entidades/acciones/UsuarioController.java
+++ src/java/sv/edu/entidades/acciones/UsuarioController.java
@@ -5,9 +5,15 @@
 package sv.edu.entidades.acciones;
 
 
+import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import sv.edu.entidades.Empleado;
+import sv.edu.entidades.Socio;
+import sv.edu.entidades.acciones.util.JsfUtil;
+import sv.edu.entidades.controladores.EmpleadoFacade;
+import sv.edu.entidades.controladores.SocioFacade;
 
 /**
  *
@@ -15,11 +21,63 @@ import javax.inject.Named;
  */
 @Named(value = "usuarioController")
 @ViewScoped
-public class UsuarioController {
+public class UsuarioController implements Serializable{
     @Inject
-    private SocioController socioController;
+    private SocioFacade socioFacade;
     @Inject
-    private EmpleadoController empleadoController;
+    private EmpleadoFacade empleadoFacade;
+    
+    private String usuario;
+    private String contrasenia;
+    private Object usuarioEnSesion;
+    
+    
+    public String validarUsuario(){
+        try{
+            Socio s = socioFacade.obtenerPorUsuarioContrasenia(usuario, contrasenia);
+            if(s!= null){
+                usuarioEnSesion = s;
+            }else{
+                Empleado e = empleadoFacade.obtenerPorUsuarioContrasenia(usuario, contrasenia);
+                if(e!=null){
+                    usuarioEnSesion = e;
+                }else{
+                    JsfUtil.addErrorMessage("Usuario o contraseña invalidos");
+                    return "inicioSesion";
+                }
+            }
+            return "bienvenido";
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+    
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public Object getUsuarioEnSesion() {
+        return usuarioEnSesion;
+    }
+
+    public void setUsuarioEnSesion(Object usuarioEnSesion) {
+        this.usuarioEnSesion = usuarioEnSesion;
+    }
+    
     
     
 }
--- src/java/sv/edu/entidades/controladores/EmpleadoFacade.java
+++ src/java/sv/edu/entidades/controladores/EmpleadoFacade.java
@@ -9,7 +9,9 @@ package sv.edu.entidades.controladores;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import sv.edu.entidades.Empleado;
+import sv.edu.entidades.Socio;
 
 /**
  *
@@ -25,6 +27,21 @@ public class EmpleadoFacade extends AbstractFacade<Empleado> {
         return em;
     }
 
+    public Empleado obtenerPorUsuarioContrasenia(String usuario, String contrasenia){
+        String jpql = "select e from Empleado e where e.usuario = :usuario and e.contrasenia= :contrasenia";
+        Empleado em = null;
+        try{
+            Query q  = getEntityManager().createQuery(jpql);
+            q.setParameter("usuario", usuario);
+            q.setParameter("contrasenia", contrasenia);
+            em=(Empleado)q.getSingleResult();
+        }catch(javax.persistence.NoResultException ne){}
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return em;
+    }
+    
     public EmpleadoFacade() {
         super(Empleado.class);
     }
